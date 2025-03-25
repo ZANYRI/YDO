d37e3a7e5990ab2ecaa3746adde053c698d817c6
ZANYRI <zanyri880@gmail.com>
2025-03-25 23:47:22 +0300

    hihihaha

---
+++ server/test.go
@@ -0,0 +1,15 @@
+package server
+
+import(
+	"github.com/gofiber/fiber/v2"
+)
+
+func Test() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":3000")
+}
